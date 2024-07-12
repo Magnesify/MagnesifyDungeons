e1cac2bcf5b98a4398db3f49090f8667702ff251
Hacı Mert Gökhan <hacimertgokhan@gmail.com>
2024-07-12 15:06:51 +0300

    0.0.0

---
+++ src/main/java/com/magnesify/magnesifydungeons/MagnesifyDungeons.java
@@ -0,0 +1,17 @@
+package com.magnesify.magnesifydungeons;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MagnesifyDungeons extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
