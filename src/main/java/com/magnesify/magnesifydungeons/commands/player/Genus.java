40c840f5b6ef873dae44185b873a8fad7efa9f23
Hacı Mert Gökhan <hacimertgokhan@gmail.com>
2024-08-30 12:12:37 +0300

    0.0.26 - Added return human function.

--- src/main/java/com/magnesify/magnesifydungeons/commands/player/Genus.java
+++ src/main/java/com/magnesify/magnesifydungeons/commands/player/Genus.java
@@ -1,6 +1,7 @@
 package com.magnesify.magnesifydungeons.commands.player;
 
 import com.magnesify.magnesifydungeons.MagnesifyDungeons;
+import com.magnesify.magnesifydungeons.dungeon.entitys.DungeonPlayer;
 import com.magnesify.magnesifydungeons.genus.gui.GenusGuiLoader;
 import com.magnesify.magnesifydungeons.genus.gui.IAGenusGuiLoader;
 import com.magnesify.magnesifydungeons.languages.LanguageFile;
@@ -21,15 +22,46 @@ public class Genus implements CommandExecutor {
         if (commandSender instanceof Player) {
             Player player = (Player) commandSender;
             boolean textc = get().getConfig().isSet("settings.genus.custom-gui-texture");
-            if(textc) {
-                if(Bukkit.getPluginManager().getPlugin("ItemsAdder") != null) {
-                    IAGenusGuiLoader.openInventory(player);
+            if(strings.length == 0) {
+
+                if(textc) {
+                    if(Bukkit.getPluginManager().getPlugin("ItemsAdder") != null) {
+                        IAGenusGuiLoader.openInventory(player);
+                    } else {
+                        Bukkit.getConsoleSender().sendMessage(parseHexColors(new LanguageFile().getLanguage().getString("plugin.error.ia.genus")));
+                        GenusGuiLoader.openInventory(player);
+                    }
                 } else {
-                    Bukkit.getConsoleSender().sendMessage(parseHexColors(new LanguageFile().getLanguage().getString("plugin.error.ia.genus")));
                     GenusGuiLoader.openInventory(player);
                 }
+            } else if (strings.length == 1) {
+                if(strings[0].equalsIgnoreCase("return-human")) {
+                    player.setMaxHealth(20.0);
+                    player.setWalkSpeed(0.4f);
+                    new DungeonPlayer(player).messageManager().actionbar((new LanguageFile().getLanguage().getString("messages.genus.returned-human")));
+                } else {
+                    if(textc) {
+                        if(Bukkit.getPluginManager().getPlugin("ItemsAdder") != null) {
+                            IAGenusGuiLoader.openInventory(player);
+                        } else {
+                            Bukkit.getConsoleSender().sendMessage(parseHexColors(new LanguageFile().getLanguage().getString("plugin.error.ia.genus")));
+                            GenusGuiLoader.openInventory(player);
+                        }
+                    } else {
+                        GenusGuiLoader.openInventory(player);
+                    }
+                }
             } else {
-                GenusGuiLoader.openInventory(player);
+                if(textc) {
+                    if(Bukkit.getPluginManager().getPlugin("ItemsAdder") != null) {
+                        IAGenusGuiLoader.openInventory(player);
+                    } else {
+                        Bukkit.getConsoleSender().sendMessage(parseHexColors(new LanguageFile().getLanguage().getString("plugin.error.ia.genus")));
+                        GenusGuiLoader.openInventory(player);
+                    }
+                } else {
+                    GenusGuiLoader.openInventory(player);
+                }
             }
         }
         return false;
